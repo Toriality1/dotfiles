ad3ab4dcb7bc2f4a89fb7862009ec10c2f66ff5a
Pedro Crelier <pcrelier@hotmail.com>
2025-06-16 18:02:47 -0300

    .

---
+++ .config/nvim/lua/toriality/lazy/discord.lua
@@ -0,0 +1,10 @@
+return {
+    'andweeb/presence.nvim',
+    config = function ()
+        require("presence").setup({
+            neovim_image_text = "Better than VSCode"
+        })
+    end
+}
+
+
