5804d3cab965a374f79d5e453593c2132b3a433e
Pedro Crelier <pcrelier@hotmail.com>
2025-06-23 01:55:37 -0300

    .

--- .config/nvim/lua/toriality/lazy/harpoon.lua
+++ .config/nvim/lua/toriality/lazy/harpoon.lua
@@ -1,7 +1,7 @@
 return
 {
     "ThePrimeagen/harpoon",
-    branch = "harpoon3",
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
