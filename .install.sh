290452f07d7677c37da72840e823834695f69a66
Pedro Crelier <torialitym@gmail.com>
2025-10-02 00:25:37 -0300

    .

--- .install.sh
+++ .install.sh
@@ -89,6 +89,10 @@ sudo apt install -y unzip
 log "Installing imagemagick..."
 sudo apt install -y imagemagick
 
+# Installing build-essential
+log "Installing build-essential..."
+sudo apt install -y build-essential
+
 # Get my dotfiles!
 log "Cloning dotfiles..."
 if [ -d "$DOTFILES" ]; then
@@ -233,6 +237,22 @@ log "NodeJS and pnpm installed successfully"
 log "Installing python3..."
 sudo apt install -y python3
 log "python3 installed successfully"
+
+# Install docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # End
 log "Setup complete! It's recommended to reboot your system."
